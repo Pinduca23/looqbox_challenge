2e269300e82e6f43807b62dc8a9109e4a54ee0c5
Luis Felipe Meinert <lfmeinert@hotmail.com>
2021-11-11 21:55:17 -0300

    Looqbox_challenge lets go python !

---
+++ looqbox_challenge.py
@@ -0,0 +1,7 @@
+import pandas 
+import numpy
+import mysql.connector
+
+create_database = pandas.DataFrame([['comida', 'bebida', 'sexo'], ['seugndo set','relou','teste1','teste 2']])
+print(create_database)
+
